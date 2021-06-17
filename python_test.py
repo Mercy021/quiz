618d49ea631b63082aa46b20841a5ceb89ff5215
mercy021 <kinyanjuimercywangari@gmail.com>
2021-06-17 10:55:36 +0300

    first commit

---
+++ python_test.py
@@ -0,0 +1,49 @@
+# # # quiz 1
+x=[100,110,120,130,140,150]
+y=[s*5 for s in x]
+print(x)
+#  quiz 7
+def greeting(age,name):
+    year=2021-age
+    print ("hello{} your born in year {} ".format(age,name))
+greeting("mercy",21)
+
+
+# # quiz 6
+def divisible_by_seven():
+    x=range(100,200)
+    for y in x:
+        if y%7==0:
+            return y
+divisible_by_seven()
+# quiz 5
+def remove():
+    x = ['a','b','a','e','d','b','c','e','f','g','h']
+    y={}
+    x.add(y)
+    y.remove(x)
+    remove()
+# quiz 4
+def smallest():
+    x=([3,6,8,2,4,1,5,7])
+    for a in x:
+        if a>=0:
+            return 1
+smallest()
+# quiz 2
+def divisible_by_three():
+    number=range(1)
+    for n in number:
+        if n%3==0:
+            return n
+divisible_by_three()
+    
+                
+            
+
+        
+    
+    
+
+    
+
